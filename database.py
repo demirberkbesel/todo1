7cb554d43b00e09421fc92e4589f56df0adaed73
demirberkbesel <beseldemirberk@gmail.com>
2026-07-01 16:03:06 +0300

    feat: setup project structure and database models

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLite veritabanı dosyası
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+
+# Veritabanı motoru oluşturma
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+# Oturum oluşturucu
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Temel model sınıfı
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from database import Base
+
+# Todo veritabanı modeli
+class Todo(Base):
+    __tablename__ = "todos"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, index=True)
+    is_completed = Column(Boolean, default=False) # Varsayılan: Tamamlanmadı
